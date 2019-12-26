8a4d4561907dfe7685783ea79ffe28a3dd2bf34a
philippine troussel <philippine.troussel11@gmail.com>
2019-12-26 21:26:37 +0100

    created controller and views for CATERER

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,48 +1,46 @@
 class VenuesController < ApplicationController
-    def index  
-        @venues = Venue.all
-    end 
+  def index
+    @venues = Venue.all
+  end
 
-    def show
-        find_venue
-    end
+  def show
+    find_venue
+  end
 
-    def new
-        @venue = Venue.new
-    end
+  def new
+    @venue = Venue.new
+  end
 
-    def create
-        @venue = Venue.new(venue_params)
-        @venue.user = current_user
-        @venue.save!
-        redirect_to venue_path(@venue)
-    end
+  def create
+    @venue = Venue.new(venue_params)
+    @venue.user = current_user
+    @venue.save!
+    redirect_to venue_path(@venue)
+  end
 
-    def edit
-        find_venue
-    end
+  def edit
+    find_venue
+  end
 
-    def update
-        find_venue
-        @venue.update(venue_params)
-        redirect_to venue_path(@venue)
-    end
+  def update
+    find_venue
+    @venue.update(venue_params)
+    redirect_to venue_path(@venue)
+  end
 
-    def destroy
-        find_venue
-        @venue.destroy
-        redirect_to venues_path
-    end
+  def destroy
+    find_venue
+    @venue.destroy
+    redirect_to venues_path
+  end
 
-    private
+  private
 
-    def find_venue
-        @venue = Venue.find(params[:id])
-    end
+  def find_venue
+    @venue = Venue.find(params[:id])
+  end
 
-    def venue_params
-        params.require(:venue).permit(:name, :description, :hourly_price, :capacity, :address)
-    end
-
-
+  def venue_params
+    params.require(:venue).permit(:name, :description, :hourly_price, :capacity, :address)
+  end
 end
