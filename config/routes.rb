481fc6ff579cf04fdf1a14ac5630b459b34a2432
Joseph Hassett <jchassett818@gmail.com>
2019-12-29 20:38:10 -0500

    Added index.html.erb and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :venues do
     resource :bookings, only: [:create] 
+    resources :favorited_venues, only: [:create, :destroy]
   end
 
   resources :bookings, only: [:index, :edit, :show]
+  resources :favorited_venues, only: [:index]
 end
