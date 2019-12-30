2e52bc412e0c7d904706de784eb73376686629bb
Joseph Hassett <jchassett818@gmail.com>
2019-12-29 20:44:05 -0500

    Fixed class syntax on venue amenity controller

--- app/controllers/venue_amenities_controller.rb
+++ app/controllers/venue_amenities_controller.rb
@@ -1,11 +1,11 @@
 class VenueAmenitiesController < ApplicationController
     def index
-        @venue_amenities = Venue_amenity.where((venue_amenity.venue = params(:venue_id)))
+        @venue_amenities = VenueAmenity.where((venue_amenity.venue = params(:venue_id)))
     end
 
     def create
         @venue = Venue.find(params[:venue_id])
-        @venue_amenity = Venue_amenity.new(venue_amenity_params)
+        @venue_amenity = VenueAmenity.new(venue_amenity_params)
         @venue_amenity.venue = @venue
         @venue_amenity.save!
         redirect_to venue_path(@venue)
@@ -20,7 +20,7 @@ class VenueAmenitiesController < ApplicationController
     private
 
     def find_venue_amenity
-        @venue_amenity = Venue_amenity.find(params[:id])
+        @venue_amenity = VenueAmenity.find(params[:id])
     end
 
     def venue_image_params
