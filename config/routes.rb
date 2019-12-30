b35fd023455752b65ca80a23f75cddbbb2587977
Joseph Hassett <jchassett818@gmail.com>
2019-12-29 20:57:47 -0500

    Added routes for caterers and performers

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   resources :bookings, only: [:index, :edit, :show]
   resources :favorited_venues, only: [:index]
+
+  resources :caterers
+  resources: performers
 end
